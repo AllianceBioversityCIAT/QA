8850b3f7624f301ddf4c861aeac107c74f33aba0
Juan Delgado <juandelgadog98@gmail.com>
2022-11-24 15:23:09 -0500

    :recycle: refactor(migration): move migrations to migrations_runned folder

--- CGIAR-QA-back/src/migration/1669318510883-tpbCommentEntity.ts
+++ CGIAR-QA-back/src/migration/1669318510883-tpbCommentEntity.ts
@@ -5,7 +5,6 @@ export class tpbCommentEntity1669318510883 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query("ALTER TABLE `qa_comments` ADD `tpb` tinyint NULL DEFAULT 0", undefined);
-        await queryRunner.query("ALTER TABLE `qa_evaluations` CHANGE `batchDate` `batchDate` datetime NOT NULL DEFAULT NOW()", undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
