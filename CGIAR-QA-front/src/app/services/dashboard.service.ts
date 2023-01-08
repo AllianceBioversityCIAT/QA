1be8f095f5792ba9e537d8589f7c7af9acf465bf
santiagoSS99 <santiagoxq@icloud.com>
2023-01-08 09:14:54 -0500

    :recycle: Refactor(Dashboard Service): Fix url endpoint

--- CGIAR-QA-front/src/app/services/dashboard.service.ts
+++ CGIAR-QA-front/src/app/services/dashboard.service.ts
@@ -37,7 +37,7 @@ export class DashboardService {
   getAllDashboardEvaluations(crp_id?) {
     let params = new HttpParams().set('crp_id', crp_id)
     // params.set('is_crp', is_crp);
-    return this.http.get<any>(`${environment.apiUrl}/evaluation`, { params });
+    return this.http.get<any>(`${environment.apiUrl}/evaluation/`, { params });
   }
   // get all dash data by crp (evaluations)
   getAllDashboardEvaluationsByCRP(crp_id?) {
