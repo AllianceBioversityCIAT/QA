97373e57b96bc4e5665411c2988a9f832c901145
santiagoSS99 <santiagoxq@icloud.com>
2023-01-23 11:25:01 -0500

    :recycle: Refactor(Icon edit): icon was moved to the name side

--- CGIAR-QA-front/src/app/crp/crp-dashboard/status-chart/status-chart.component.ts
+++ CGIAR-QA-front/src/app/crp/crp-dashboard/status-chart/status-chart.component.ts
@@ -5,7 +5,7 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './status-chart.component.html',
   styleUrls: ['./status-chart.component.scss']
 })
-export class StatusChartComponent implements OnInit{
+export class StatusChartComponent implements OnInit {
   @Input() indicator;
   @Input() indicators;
   @Input() total;
@@ -13,8 +13,8 @@ export class StatusChartComponent implements OnInit{
   view: any[] = [700, 400];
 
   legendLabels = [
-    {name:"Answered / No action needed", class: "answered", value: 0},
-    {name:"Pending", class:"pending", value: 0 },
+    { name: "Answered / No action needed", class: "answered", value: 0 },
+    { name: "Pending", class: "pending", value: 0 },
   ];
 
 
@@ -35,7 +35,7 @@ export class StatusChartComponent implements OnInit{
 
   constructor() {
 
-    
+
   }
 
   ngOnInit() {
@@ -47,26 +47,26 @@ export class StatusChartComponent implements OnInit{
   }
 
   formatIndicator() {
-    this.results = [{name: this.indicator[0].name, series: []}];
+    this.results = [{ name: this.indicator[0].name, series: [] }];
     this.indicator[0].series.forEach(element => {
       // this.statusChartData[indicator][element.status] = +element.value;
       let status = element.status == 'complete' ? 'Answered / No action needed' : 'Pending';
       this.legendLabels.find(el => el.name == status).value = element.value;
-      this.results[0].series.push({ name: element.status == 'complete' ? 'Answered / No action needed' : 'Pending', value: +element.value })  
-      
+      this.results[0].series.push({ name: element.status == 'complete' ? 'Answered / No action needed' : 'Pending', value: +element.value })
+
     });
     this.results[0].series.reverse();
     console.log(this.results[0].series);
     const isAllPending = this.results[0].series.find(el => el.name == 'Answered / No action needed' && el.value == 0);
-    console.log({isAllPending});
-    
+    console.log({ isAllPending });
 
-    if(this.results[0].series.find(el => el.name == 'Pending' && el.value == this.total)) this.colorScheme.domain.shift();
+
+    if (this.results[0].series.find(el => el.name == 'Pending' && el.value == this.total)) this.colorScheme.domain.shift();
   }
 
   indicatorIsEnable() {
     // console.log(this.indicator[0].name);
-    
+
     return this.indicators.find(indicator => indicator.view_name == this.indicator[0].name).comment_meta.enable_crp;
   }
 
