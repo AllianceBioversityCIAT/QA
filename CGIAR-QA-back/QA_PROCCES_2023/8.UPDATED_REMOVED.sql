e607706d070215cc87bbb090841c2ce080b30261
Juan Delgado <juandelgadog98@gmail.com>
2023-11-24 08:45:55 -0500

    :recycle: refactor(QA 2023): Fix SQL query and update HTML template

--- CGIAR-QA-back/QA_PROCCES_2023/8.UPDATED_REMOVED.sql
+++ CGIAR-QA-back/QA_PROCCES_2023/8.UPDATED_REMOVED.sql
@@ -181,3 +181,25 @@ WHERE
 			AND e.indicator_view_id = o.id
 			AND o.phase_year = 2023
 	);
+
+-- 9 IPSR
+UPDATE
+	qa_evaluations e
+SET
+	evaluation_status = 'Removed'
+WHERE
+	e.indicator_view_name = 'qa_innovation_use_ipsr'
+	AND evaluation_status != 'Removed'
+	AND not EXISTS (
+		SELECT
+			1
+		FROM
+			qa_innovation_use_ipsr_view o
+		WHERE
+			o.phase_name = 'AR'
+			AND o.included_AR = 'yes'
+			AND o.is_active = 1
+			AND o.submitted = 1
+			AND e.indicator_view_id = o.id
+			AND o.phase_year = 2023
+	);
