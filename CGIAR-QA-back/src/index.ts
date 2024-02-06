418fa90a415f629cd8b7a62377cedb1f74129323
Juan Delgado <juandelgadog98@gmail.com>
2024-02-06 10:04:43 -0500

    :recycle: refactor(CORS policy): Updated policy to allow the control check from origin.  P2-342

--- CGIAR-QA-back/src/index.ts
+++ CGIAR-QA-back/src/index.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-require('module-alias/register')
+require("module-alias/register");
 import { createConnection, ConnectionManager, Connection } from "typeorm";
 import express from "express";
 import { Response } from "express";
@@ -8,52 +8,42 @@ import helmet from "helmet";
 import cors from "cors";
 import Routes from "./routes/IndexRoute";
 import config from "./config/config";
-const { handleError } = require('./_helpers/ErrorHandler');
-const parentDir = require('path').resolve(process.cwd(), '../');
-const morgan = require('morgan');
+const { handleError } = require("./_helpers/ErrorHandler");
+const parentDir = require("path").resolve(process.cwd(), "../");
+const morgan = require("morgan");
 
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
-// Connects to the Database -> then starts the express
 createConnection()
-    .then(() => {
-        // Create a new express application instance
-        const app = express();
-        // Call midlewares
-        app.use(morgan('dev'))
-        app.use(cors());
-        app.use(helmet(
-            { frameguard: false }
-        ));
-        app.use(bodyParser.json());
-        // app.use(express.static(parentDir + '/CGIAR-QA-front/dist/qa-app'));
+  .then(() => {
+    const app = express();
+    app.use(morgan("dev"));
+    app.use(cors(corsOptions));
+    app.use(helmet({ frameguard: false }));
+    app.use(bodyParser.json());
 
-
-
-        //Set all routes from routes folder
-        app.use("/api", Routes);
-        app.get("/", (req, res) => {
-            res.status(200).json({ msg: 'Welcome to QA API! 🖥' });
-        });
-        // app.get('/', (req, res) => {
-        //     res.sendFile(parentDir + "/CGIAR-QA-front/dist/qa-app/index.html")
-        // });
-        // // 404 catch 
-        // app.all('*', (req: any, res: any) => {
-        //     console.log(`[TRACE] Server 404 request: ${req.originalUrl}`);
-        //     res.status(200).sendFile(parentDir + "/CGIAR-QA-front/dist/qa-app/index.html");
-        // });
-
-        //Handle Errors
-        app.use((err, req, res, next) => {
-            res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
-            res.setHeader("Content-Type", "application/json; charset=utf-8");
-            handleError(err, res);
-        });
-        let server = app.listen(config.port, config.host, () => {
-            console.log(`Current parent directory: ${parentDir} `);
-            console.log(`Server started on port ${config.port} and host ${config.host}!`);
-        });
-    })
-    .catch(error => {
-        console.log('createConnection', error)
+    app.use("/api", Routes);
+    app.get("/", (req, res) => {
+      res.status(200).json({ msg: "Welcome to QA API! 🖥" });
     });
+
+    // app.use((err, req, res, next) => {
+    //   res.setHeader("Cross-Origin-Resource-Policy", "same-site");
+    //   res.setHeader("Content-Type", "application/json; charset=utf-8");
+    //   handleError(err, res);
+    // });
+    let server = app.listen(config.port, config.host, () => {
+      console.log(`Current parent directory: ${parentDir} `);
+      console.log(
+        `Server started on port ${config.port} and host ${config.host}!`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log("createConnection", error);
+  });
