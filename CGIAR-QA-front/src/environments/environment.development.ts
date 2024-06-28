2f1108825b29c0d7355a74dba215852e647336b0
Juan Delgado <juandelgadog98@gmail.com>
2024-06-28 10:54:48 -0500

    ✨ feat(QA Front): Updated Angular to latest version
    Co-Authored-By: Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>

---
+++ CGIAR-QA-front/src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+    apiUrl: `http://localhost:3000/api`,
+    tawkToId: '5ea2f28869e9320caac6f637',
+    ga: 'UA-167825249-2',
+    prUrl: `https://prtest.ciat.cgiar.org/`,
+    phase: 18,
+    footerUrls: {
+      termsAndCondition:
+        'https://cgiar-prms.notion.site/Legal-Terms-eb2f3e436f3e46bd9057b1cb1e3391b8',
+      license:
+        'https://github.com/AllianceBioversityCIAT/onecgiar_pr/blob/master/LICENSE',
+    },
+  };
+  
---
+++ CGIAR-QA-front/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  apiUrl: `http://localhost:3000/api`,
+  tawkToId: '5ea2f28869e9320caac6f637',
+  ga: 'UA-167825249-2',
+  prUrl: `https://prtest.ciat.cgiar.org/`,
+  phase: 18,
+  footerUrls: {
+    termsAndCondition:
+      'https://cgiar-prms.notion.site/Legal-Terms-eb2f3e436f3e46bd9057b1cb1e3391b8',
+    license:
+      'https://github.com/AllianceBioversityCIAT/onecgiar_pr/blob/master/LICENSE',
+  },
+};
