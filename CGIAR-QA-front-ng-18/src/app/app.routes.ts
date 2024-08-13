781303dc2d156bb21483a2199e8a4ef82b0a7073
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-08-13 15:31:11 -0500

    :sparkles: feat: Login front added

--- CGIAR-QA-front-ng-18/src/app/app.routes.ts
+++ CGIAR-QA-front-ng-18/src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadComponent: () => import('./pages/core/core.component'),
+  //   children: [
+  //     {
+  //       path: '',
+  //       redirectTo: 'sites-slider',
+  //       pathMatch: 'full'
+  //     },
+  //     {
+  //       path: 'sites-slider',
+  //       loadComponent: () => import('./pages/core/pages/sites-slider/sites-slider.component')
+  //     },
+  //     {
+  //       path: 'sites-list',
+  //       loadComponent: () => import('./pages/core/pages/sites-list/sites-list.component')
+  //     },
+  //     {
+  //       path: 'settings',
+  //       loadComponent: () => import('./pages/core/pages/settings/settings.component')
+  //     }
+  //   ]
+  // }
+  {
+    path: '',
+    loadComponent: () => import('./pages/login/login.component')
+  }
+  // {
+  //   path: 'fields',
+  //   loadComponent: () => import('./pages/dynamic-fields/dynamic-fields.component')
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: 'not-found',
+  //   pathMatch: 'full'
+  // }
+];
