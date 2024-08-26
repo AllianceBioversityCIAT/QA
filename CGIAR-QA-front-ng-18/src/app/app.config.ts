441c9bacf5ac74737c2e44015dc7f0aec9658433
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-08-26 10:57:35 -0500

    :sparkles: feat: Login endpoint added and  consumed

--- CGIAR-QA-front-ng-18/src/app/app.config.ts
+++ CGIAR-QA-front-ng-18/src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), importProvidersFrom(BrowserModule,BrowserAnimationsModule )]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withViewTransitions()), provideHttpClient(), importProvidersFrom(BrowserModule, BrowserAnimationsModule)]
 };
--- CGIAR-QA-front-ng-18/src/app/pages/login/login.component.ts
+++ CGIAR-QA-front-ng-18/src/app/pages/login/login.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { ApiService } from '../../services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,4 +13,13 @@ import { CardModule } from 'primeng/card';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export default class LoginComponent {}
+export default class LoginComponent {
+  api = inject(ApiService);
+  router = inject(Router);
+
+  onLogin = async () => {
+    const res = await this.api.login();
+    if (res.successfulRequest) this.router.navigate(['/home']);
+    console.log(res);
+  };
+}
