5bb16933e39da1a345e286d82eb184c9bb59343f
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-10-03 09:36:52 -0500

    :sparkles: feat: Old routing comments added

--- CGIAR-QA-front-ng-18/src/app/app.routes.ts
+++ CGIAR-QA-front-ng-18/src/app/app.routes.ts
@@ -21,10 +21,57 @@ export const routes: Routes = [
   {
     path: 'crp',
     loadComponent: () => import('./pages/crp/crp.component')
+    // children: [
+    //   {
+    //     path: 'dashboard',
+    //     loadComponent: () => import('./pages/crp-dashboard/crp-dashboard.component')
+    //     // canActivate: [AuthGuard],
+    //     // data: { roles: [Role.crp, Role.admin] },
+    //     // component: CrpDashboardComponent,
+    //   },
+    //   {
+    //     path: 'indicator/:type/:primary_column',
+    //     loadComponent: () => import('./pages/detail-indicator/detail-indicator.component'),
+    // children:[
+    //   {
+    //     path: '',
+    //     canActivate: [AuthGuard],
+    //     data: { roles: [Role.crp, Role.admin] },
+    //     component: CRPIndicatorsComponent,
+    // },
+    // {
+    //     path: 'detail/:indicatorId',
+    //     component: DetailIndicatorComponent
+    // },
+    // ]
+    //   }
+    // ]
   },
   {
     path: 'indicator/:type/:primary_column',
     loadComponent: () => import('./pages/indicator/indicator.component')
+    // children: [
+    //   {
+    //     path: '',
+    //     component: IndicatorsComponent,
+    //     canActivate: [AuthGuard, AvailableGuard],
+    //     data: { roles: [Role.asesor, Role.admin] },
+    //     // pathMatch: 'full'
+    // },
+    // {
+    //     path: 'detail/:indicatorId',
+    //     loadChildren: () => import('./general-detailed-indicator/general-detailed-indicator.module').then(mod => mod.GeneralDetailedIndicatorModule),
+    //     children: [
+    //       {
+    //         path: '',
+    //         component: GeneralDetailedIndicatorComponent,
+    //         canActivate: [AuthGuard],
+    //         data: { roles: [Role.asesor, Role.admin] },
+
+    //     },
+    //     ]
+    // }
+    // ]
   },
   { path: 'qa-close', loadComponent: () => import('./pages/qa-close/qa-close.component') },
   { path: 'login', loadComponent: () => import('./pages/login/login.component') },
