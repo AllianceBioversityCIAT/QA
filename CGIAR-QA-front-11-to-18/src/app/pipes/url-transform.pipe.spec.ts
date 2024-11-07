91c27b761c7f7475d52df371dbf9265223574f0a
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-11-07 08:45:04 -0500

    :recycle: refactor: CGIAR-QA-front-11-to-18 removed

--- CGIAR-QA-front-11-to-18/src/app/pipes/url-transform.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UrlTransformPipe } from './url-transform.pipe';
-
-describe('UrlTransformPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UrlTransformPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
