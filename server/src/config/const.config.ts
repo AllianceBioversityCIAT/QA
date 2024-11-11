7186ccb8e1c1dc4e1a7fc2510636b2687fc9797f
Juan D. Guzmán <juandelgadog98@gmail.com>
2024-11-11 12:02:15 -0500

    ♻️ refactor(AD): Change Active Directory configuration

--- server/src/config/const.config.ts
+++ server/src/config/const.config.ts
@@ -1,11 +1,13 @@
+import { env } from 'process';
+
 export default {
   jwtSecret: '@QEGTUI',
   port: 3000,
   jwtTime: '5h',
   host: '0.0.0.0',
   active_directory: {
-    url: 'ldap://ciatroot3.cgiarad.org:3268',
-    baseDN: 'DC=CGIARAD, DC=ORG',
-    domain: 'cgiar.org',
+    url: env.AD_URL,
+    baseDN: env.AD_BASEDN,
+    domain: env.AD_DOMAIN,
   },
 };
