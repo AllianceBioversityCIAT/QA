0963b54ab48c81f3afed598f83cc46aa5a30f004
Juan D. Guzmán <juandelgadog98@gmail.com>
2026-01-30 11:27:57 -0500

    ♻️ Refactor AI helper module by removing unused imports and middleware
    Streamlined the AI helper module by eliminating unnecessary imports and middleware configuration, simplifying the module structure while retaining essential functionality. This refactor enhances code clarity and maintainability.

--- server/src/api/ai-helper/ai-helper.module.ts
+++ server/src/api/ai-helper/ai-helper.module.ts
@@ -1,47 +1,10 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AiHelperService } from './ai-helper.service';
 import { AiHelperController } from './ai-helper.controller';
 import { AiHelperRepository } from './ai-helper.repository';
-import { JwtMiddleware } from '../../shared/middlewares/jwt.middleware';
-import { JwtService } from '@nestjs/jwt';
-import { BcryptPasswordEncoder } from '../../utils/bcrypt.utils';
-import { AuthService } from '../auth/auth.service';
-import { TokenAuthRepository } from '../auth/repositories/token-auth.repository';
-import { UserRepository } from '../users/users.repository';
-import { GeneralConfigurationRepository } from '../../shared/repositories/general-config.repository';
-import { CycleRepository } from '../../shared/repositories/cycle.repository';
-import { CrpRepository } from '../../shared/repositories/crp.repository';
-import { RoleRepository } from '../roles/repositories/role.repository';
-import { UserRoleRepository } from '../users/user-role.repository';
-import { AuthMicroserviceModule } from '../../shared/microservice/auth-microservice/auth-microservice.module';
 
 @Module({
   controllers: [AiHelperController],
-  providers: [
-    AiHelperService,
-    AiHelperRepository,
-    JwtService,
-    AuthService,
-    BcryptPasswordEncoder,
-    TokenAuthRepository,
-    UserRepository,
-    GeneralConfigurationRepository,
-    CycleRepository,
-    CrpRepository,
-    UserRoleRepository,
-    RoleRepository,
-  ],
-  imports: [AuthMicroserviceModule],
+  providers: [AiHelperService, AiHelperRepository],
 })
-export class AiHelperModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(JwtMiddleware)
-      .forRoutes({ path: 'api/ai-helper', method: RequestMethod.ALL });
-  }
-}
+export class AiHelperModule {}
